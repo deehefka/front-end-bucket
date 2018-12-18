14cc1524a0c1f8bbfb248d3bebadf1bad452f6ba
CodingAlchemist18 <physicsnerd38@gmail.com>
2018-12-18 14:52:31 -0500

    hide and show auth buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,15 +7,30 @@
 // require('./example')
 
 const authEvents = require('./event.js')
+const store = require('./store.js')
 
 // const JS = require('./JS.js')
 
 $(() => {
-  // your JS code goes here
+  store.userSignedIn = false
+  if (store.userSignedIn === false) {
+    $('#sign-up-btn').show()
+    $('#sign-in-btn').show()
+    $('#sign-out-btn').hide()
+    $('#password-btn').hide()
+  }
+  // Auth CRUD Actions
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
+<<<<<<< HEAD
   $('#sign-out-form').on('click', authEvents.onSignOut)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
+=======
+  $('#sign-out-form').on('submit', authEvents.onSignOut)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+
+  // Bucket List CRUD Actions
+>>>>>>> hide and show auth buttons
   $('#bucket-list-create').on('submit', authEvents.onTodoListCreate)
   $('#bucket-list-index').on('click', authEvents.onTodoListIndex)
   $('#bucket-list-delete').on('submit', authEvents.onTodoListDelete)
