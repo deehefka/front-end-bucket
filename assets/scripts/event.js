8d582724555cae97799de07ef8625336b529dfbc
deehefka <43755698+deehefka@users.noreply.github.com>
2018-12-20 16:38:36 -0500

    get form fields location

--- assets/scripts/event.js
+++ assets/scripts/event.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const getFormFields = require('./../../lib/get-form-fields.js')
+const getFormFields = require('../../lib/get-form-fields.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
 
